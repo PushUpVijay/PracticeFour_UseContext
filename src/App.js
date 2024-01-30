0c28ed074642b7d5ad3d7aad947a92b1928d0d53
PushUpVijay <59018547+PushUpVijay@users.noreply.github.com>
2024-01-30 16:01:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import {useState,createContext,useContext} from "react";
+const ThemeContext=createContext(null);
+function App() {
+  const [theme,setTheme]=useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <Form/>
+      <label>
+        Use Dark Theme
+      </label>
+      <input type="checkbox" checked={theme==='dark'} onChange={(e)=>{setTheme(e.target.checked? 'dark':'light')}}/>
+    </ThemeContext.Provider>
+  );
+}
+function Form(){
+  return(
+    <Panel title="Welcome">
+      <Button>Sign Up</Button>
+      <Button>Sign In</Button>
+    </Panel>
+  )
+}
+function Panel({title,children}){
+  const theme=useContext(ThemeContext);
+  const className='panel-'+theme;
+  return(
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
+function Button({children}){
+  const theme=useContext(ThemeContext);
+  const className='button-'+theme;
+  return(
+    <button className={className}>{children}</button>
+  )
+}
+export default App;
